45c4ed83a2b4c5c324f65ce7ee12d4b442dd2c68
Thinh_Nguyen <thinhnguyen042002@gmail.com>
2024-05-15 22:56:36 +1000

    Adding more test case and test runner

--- test_runner.py
+++ test_runner.py
@@ -5,18 +5,22 @@ from utils import expr
 
 METHODS = ['TT', 'DPLL', 'BC', 'FC']
 
+content = ''
+content = 'No, test case,' + ', '.join(METHODS) + '\n'
+
 for root, dirs, files in os.walk('tests/'):
-    for file in files:
+    for index, file in enumerate(files):
+        line = str(index + 1) + ', '
         if file.endswith('.txt'):
             file_path = os.path.join(root, file)
-            print(file_path)
+            line += file_path + ', '
             tells, ask = extract_input_file(file_path)
-            result = ''
+            result = []
             for method in METHODS:
                 kb = None
                 if 'generic' in file_path.lower():
                     if method not in ['TT', 'DPLL']:
-                        result += 'N/A '
+                        result.append('N/A')
                         continue
                     kb = PropKB()
                 elif 'horn' in file_path.lower():
@@ -27,17 +31,22 @@ for root, dirs, files in os.walk('tests/'):
                     kb.tell(expr(tell))
 
                 if (method == 'TT'):
-                    result += ('YES' if kb.ask_generator_tt(expr(ask))[0] else 'NO') + ' '
+                    result.append('YES' if kb.ask_generator_tt(expr(ask))[0] else 'NO')
                 elif (method == 'BC'):
                     assert(isinstance(kb, PropDefiniteKB))
-                    result += ('YES' if kb.ask_generator_bc(expr(ask))[0] else 'NO') + ' '
+                    result.append('YES' if kb.ask_generator_bc(expr(ask))[0] else 'NO')
                 elif (method == 'FC'):
                     assert(isinstance(kb, PropDefiniteKB))
-                    result += ('YES' if kb.ask_generator_fc(expr(ask))[0] else 'NO') + ' '
+                    result.append('YES' if kb.ask_generator_fc(expr(ask))[0] else 'NO')
                 elif (method == 'DPLL'):
-                    result += ('YES' if kb.ask_generator_dpll(expr(ask))[0] else 'NO') + ' '
+                    result.append('YES' if kb.ask_generator_dpll(expr(ask))[0] else 'NO')
                 else:
                     raise ValueError("Invalid method")
-            print(result)
-            
+            line += ','.join(result) + '\n'
+        
+        content += line
+
+#write content to a csv file
+with open('test_results.csv', 'w') as f:
+    f.write(content)
 
